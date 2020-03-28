f01cea32588a40052c52caa71bb513332680744a
Buz <contato@gabrielhenrique.net>
2020-03-28 19:30:36 -0300

    add infinite load

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,6 +11,9 @@ import styles from './styles';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([])
   const [total, setTotal] = useState(0)
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+
   const navigation = useNavigation();
 
   function navigateToDetail(incident) {
@@ -18,10 +21,20 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const res = await api.get('incidents');
+    if (loading) return;
 
-    setIncidents(res.data)
+    if (total > 0 && incidents.length === total) return;
+
+    setLoading(true)
+
+    const res = await api.get('incidents', {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...res.data])
     setTotal(res.headers['x-total-count'])
+    setPage(page + 1)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -44,6 +57,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
