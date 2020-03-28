d36e43b5c3e183e65b018b9845f565edf03dff3d
Buz <contato@gabrielhenrique.net>
2020-03-28 16:57:23 -0300

    add navigate to detail page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -9,6 +9,10 @@ import styles from './styles';
 export default function Incidents() {
   const navigation = useNavigation();
 
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,7 +42,7 @@ export default function Incidents() {
 
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={() => { }}
+              onPress={navigateToDetail}
             >
               <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
               <Feather name="arrow-right" size={16} color="#E02041" />
